69b68c129043e04bcb626be47510dd0e6e70f035
jonahkim <jonahkim@localhost>
2020-11-19 18:03:21 -0500

    PromotionDA

--- src/dataAccess/PromotionDA.java
+++ src/dataAccess/PromotionDA.java
@@ -1,6 +1,10 @@
 package dataAccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import models.Promotion;
@@ -13,24 +17,91 @@ public class PromotionDA implements IPromotionDA {
 	
 	private static final String getAllPromotionsQuery = "SELECT * FROM Promotion";
 	
-	private static final String getPromotionByCode = "SELECT * FROM Promotion WHERE PromotionCode = ?;";
+	private static final String getPromotionByCodeQuery = "SELECT * FROM Promotion WHERE PromotionCode = ?;";
 
 	@Override
 	public void createPromotion(Promotion promotion) throws SQLException {
-		// TODO Auto-generated method stub
+		int promotionCode = promotion.getPromotionCode();
+		int percentage = promotion.getPercentage();
+		String expDate = promotion.getExpDate();
+		String startDate = promotion.getStartDate();
+		
+		Connection connection = DataAccessHelper.getConnection();
+		PreparedStatement useDBStmt = connection.prepareStatement(useDBQuery);
+		useDBStmt.executeQuery();
+		PreparedStatement addPromotionStmt = connection.prepareStatement(addPromotionQuery);
+		addPromotionStmt.setInt(1, promotionCode);
+		addPromotionStmt.setInt(2, percentage);
+		addPromotionStmt.setString(3, expDate);
+		addPromotionStmt.setString(4, startDate);
+		addPromotionStmt.executeUpdate();
+		
+		connection.close();
 
 	}
 
 	@Override
 	public List<Promotion> getAllPromotions() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Promotion> promotions = new ArrayList<Promotion>();
+		Connection connection = DataAccessHelper.getConnection();
+		PreparedStatement useDBStmt = connection.prepareStatement(useDBQuery);
+		useDBStmt.executeQuery();
+		PreparedStatement getAllPromotionsStmt = connection.prepareStatement(getAllPromotionsQuery);
+		ResultSet allPromotionsRS = getAllPromotionsStmt.executeQuery();
+		
+		while(allPromotionsRS.next()) {
+			Promotion promotion = new Promotion();
+			
+			int promotionCode = allPromotionsRS.getInt(1);
+		    int percentage = allPromotionsRS.getInt(2);
+		    String expDate = allPromotionsRS.getString(3);
+		    String startDate = allPromotionsRS.getString(4);
+		    
+		    promotion.setPromotionCode(promotionCode);
+		    promotion.setPercentage(percentage);
+		    promotion.setExpDate(expDate);
+		    promotion.setStartDate(startDate);
+		    
+		    promotions.add(promotion);
+		}
+		
+		connection.close();
+		
+		return promotions;
 	}
 
 	@Override
 	public Promotion getPromotionByCode(int promotionCode) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Promotion promotion = null;
+		Connection connection = DataAccessHelper.getConnection();
+		PreparedStatement useDBStmt = connection.prepareStatement(useDBQuery);
+		useDBStmt.executeQuery();
+		PreparedStatement getPromotionByCodePstmt = connection.prepareStatement(getPromotionByCodeQuery);
+		getPromotionByCodePstmt.setInt(1, promotionCode);    
+		
+		ResultSet promotionRS = getPromotionByCodePstmt.executeQuery();
+		    
+		while(promotionRS.next()) {
+			promotion = new Promotion();
+			
+			int dbPromotionCode = promotionRS.getInt(1);
+		    int percentage = promotionRS.getInt(2);
+		    String expDate = promotionRS.getString(3);
+		    String startDate = promotionRS.getString(4);
+		    
+		    promotion.setPromotionCode(dbPromotionCode);
+		    promotion.setPercentage(percentage);
+		    promotion.setExpDate(expDate);
+		    promotion.setStartDate(startDate);
+		    
+		    connection.close();
+		    
+		    return promotion;
+		}
+		    
+		connection.close();
+		
+		return promotion;
 	}
 
 }
